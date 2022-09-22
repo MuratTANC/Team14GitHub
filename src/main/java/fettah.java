e8184372df065c24ffc141d69d88c0c50ca4e35d
Fettah32 <adilyassikaya@gmail.com>
2022-09-22 19:45:18 +0300

    22.09.2022

--- src/main/java/fettah.java
+++ src/main/java/fettah.java
@@ -2,6 +2,6 @@ public class fettah {
     public static void main(String[] args) {
         System.out.println("Adil Fettah");
 
-        System.out.println("conflict");
+        System.out.println("asdasd");
     }
 }
